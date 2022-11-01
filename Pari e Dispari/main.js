7bbd43e69c59bfc6c97850040fa94dcc43cac3f5
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-11-01 16:27:32 +0100

    fixato check if pari o dispari

--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -5,19 +5,18 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
 
 /* FUNCTIONS */
-function dispari(num){
-    if (userNumber%2 == 0) {
-        alert("scegli un numero pari")
-        location.reload()
-    }
-    if (userNumber>5 || userNumber<1 ||userNumber==null) {
-        alert("rispetta i parametri")
-        location.reload()
-    }
-    
-}
 
-function pari(num){
+   
+    
+
+  
+
+/* VARIABILI DI SUPPORTO */
+const scelta=prompt("Pari o Dispari?")
+const userNumber=prompt("Inserisci un numero da 1 a 5")
+
+//check pari o dispari
+if (scelta.toLowerCase()== "pari") {
     if (userNumber%2 == 1) {
         alert("scegli un numero pari")
         location.reload()
@@ -27,18 +26,17 @@ function pari(num){
         location.reload()
     }
     
-}
-/* VARIABILI DI SUPPORTO */
-const scelta=prompt("Pari o Dispari?")
-
-//check pari o dispari
-if (scelta.toLowerCase()== "pari") {
-    const userNumber=prompt("Inserisci un numero da 1 a 5")
-    pari()
 }else if(scelta.toLowerCase()=="dispari"){
-    const userNumber=prompt("Inserisci un numero da 1 a 5")
-    dispari()
+    if (userNumber%2 == 0) {
+        alert("scegli un numero pari")
+        location.reload()
+    }
+    if (userNumber>5 || userNumber<1 ||userNumber==null) {
+        alert("rispetta i parametri")
+        location.reload()
+    }
 }
+
 
 const chosenNumber=pari(userNumber)
 console.log(chosenNumber)
